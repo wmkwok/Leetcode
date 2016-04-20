d428f7b610a6138ea68491d0ca7d10fb1f6229bc
wmkwok <wmkwok@indiana.edu>
2016-04-20 19:50:15 -0400

    intial e-problems

---
+++ 204_count_primes.py
@@ -0,0 +1,12 @@
+# Description:
+
+# Count the number of prime numbers less than a non-negative number, n.
+
+
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
