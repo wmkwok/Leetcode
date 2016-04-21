108f24b2e4fec05d50f38bfdbe44abfe1e75195d
wmkwok <wmkwok@indiana.edu>
2016-04-20 20:13:05 -0400

    001 finished

--- 001_two_sum.py
+++ 001_two_sum.py
@@ -1,7 +1,5 @@
 # Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
 # You may assume that each input would have exactly one solution.
-
 # Example:
 # Given nums = [2, 7, 11, 15], target = 9,
 
@@ -15,4 +13,15 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i!=j:
+                    if nums[i] + nums[j] == target:
+                        print [i, j]
+                        return [i, j]
+
+
+nums = [2, 7, 11, 15]
+target = 9
+sol = Solution()
+sol.twoSum(nums, target)
