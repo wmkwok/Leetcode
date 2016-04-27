6966955aac50aec8a817e3b7649462873c7d7581
wmkwok <wmkwok@indiana.edu>
2016-04-27 18:37:56 -0400

    remove files from outer folder

--- 231_power_of_two.py
+++
@@ -1,9 +0,0 @@
-# Given an integer, write a function to determine if it is a power of two.
-
-class Solution(object):
-    def isPowerOfTwo(self, n):
-        """
-        :type n: int
-        :rtype: bool
-        """
-        
