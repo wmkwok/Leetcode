1340645f34091d04c9d82784aef496c4446c29dc
wmkwok <wmkwok@indiana.edu>
2016-06-18 10:13:07 +0800

    reset to b7769be7dc

---
+++ 014_longest_common_prefix.py
@@ -0,0 +1,9 @@
+# Write a function to find the longest common prefix string amongst an array of strings.
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        
---
+++ 168_excel_sheet_colum_title.py
@@ -0,0 +1,19 @@
+# Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+
+# For example:
+
+#     1 -> A
+#     2 -> B
+#     3 -> C
+#     ...
+#     26 -> Z
+#     27 -> AA
+#     28 -> AB 
+
+class Solution(object):
+    def convertToTitle(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        
