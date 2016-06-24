0c48263afe50e7e5a9cb29ea73b18c9be2a07e73
wmkwok <wmkwok@indiana.edu>
2016-06-24 14:11:11 +0800

    13, 14, 19

--- python/014_longest_common_prefix.py
+++ python/014_longest_common_prefix.py
@@ -6,4 +6,18 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        
+        prefix = ""
+        for i in range(min([len(s) for s in strs])):
+            same = True
+            for s in range(len(strs) - 1):
+                if strs[s][i] != strs[s+1][i]:
+                    same = False
+            if same:
+                prefix += strs[0][i]
+            else:
+                break
+        return prefix
+            
+test = ["interstellar", "interstate", "interspecies"]
+sol = Solution()
+print sol.longestCommonPrefix(test)
--- python/020_valid_parentheses.py
+++ python/020_valid_parentheses.py
@@ -1,10 +1,35 @@
 # Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 # The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
 
+#use a stack for the first half and pair up the rest as you go
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+        if len(s) % 2 == 1:
+            return False
+        openparens = []
+        for i in range(len(s)):
+            if s[i] == '(' or s[i] == '[' or s[i] == '{':
+                openparens.append(s[i])
+                print openparens
+            elif s[i] == ')':
+                if len(openparens) == 0 or openparens.pop() != '(':
+                    return False
+            elif s[i] == ']':
+                if len(openparens) == 0 or openparens.pop() != '[':
+                    return False
+            else:
+                if len(openparens) == 0 or openparens.pop() != '{':
+                    return False
+        return True
+
+strang = "(({{}}[]))"
+strung = "()(]{}"
+sol = Solution()
+print sol.isValid(strang)
+print sol.isValid(strung)
+
+"""
+:type s: str
+:rtype: bool
+"""
         
