99bf8baf23fe888d0367f3adc724b7e73455d0e0
Wendy Kwok <wmkwok95@gmail.com>
2016-10-14 15:08:56 -0400

    070

--- python/070_climbing_stairs.py
+++ python/070_climbing_stairs.py
@@ -7,4 +7,15 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        paths = 0
+        if n == 0:
+            return 1
+        elif n < 3:
+            return n
+        else:
+            return self.climbStairs(n-1) + self.climbStairs(n-2)
+
+sol = Solution()
+for i in range(5):
+    print sol.climbStairs(i)
         
