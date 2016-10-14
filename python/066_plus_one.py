fcc9fa566791bd5715ff8691262e23f9525a338e
Wendy Kwok <wmkwok95@gmail.com>
2016-10-14 13:50:22 -0400

    066

--- python/066_plus_one.py
+++ python/066_plus_one.py
@@ -7,4 +7,14 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        
+        sum = 0
+        mult = 1
+        for i in range(len(digits)-1, -1, -1):
+            sum += mult * ((int)(digits[i]))
+            mult *= 10
+        return sum+1
+
+
+test1 = "1234567890"
+sol = Solution()
+print sol.plusOne(test1)
